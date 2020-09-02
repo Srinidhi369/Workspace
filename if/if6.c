4e9691658e1b1c631dadc79951714f84fc5f4ee3
Srinidhi369 <ksrinidhi.aithal@gmail.com>
2020-09-02 11:56:33 +0530

    if6.c

---
+++ if/if6.c
@@ -0,0 +1,30 @@
+
+
+
+
+#include<stdio.h>
+
+void main()
+{
+int month;
+	printf("Enter the month number 1 to 12 \n");
+	scanf("%d",&month);
+	if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==11)
+	{
+		printf("31 days in this month\n");
+	}	
+	else if(month==4 || month==6 || month==9 || month ==11)
+	{
+		printf("30 days in this month\n");
+	}		
+	else if(month==2)
+	{
+		printf("28 or 29 days in a month\n");
+	}
+	else 
+	{
+		printf("Enter valid number from 1 to 12:\n");	
+	}
+}
+
+		
