1b7cda2176d28805366599bd0675940fc1ba854d
Srinidhi369 <ksrinidhi.aithal@gmail.com>
2020-09-23 12:13:07 +0530

    arr6.c

---
+++ array/arr6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+	int a[10]={63,4,91,62,72,43,67,82,51,98};
+	int Even=0;
+	int i=0;
+	while(i<10)
+		{
+			if(a[i]%2==0)
+			{	
+				Even++;
+			}
+			i++;
+		}
+	printf("Even elements in array=%d\n",Even);
+}
---
+++ array/arr7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+	int a[10]={63,48,31,89,50,17,9,72,29,1};
+	int sum =0;
+	int i=0;
+	while(i<10)
+		{
+			if(a[i]%2==0)
+			{	
+				sum=sum+a[i];
+			}
+			i++;
+		}
+	printf("Sum of Even elements in array=%d\n",sum);
+}
